062e75e3e12b01f1757a890eb8536636425c7a83
Zeljko Predjeskovic <zepr2022000@gmail.com>
2021-05-15 19:50:14 +0200

    springboot app

---
+++ src/main/java/com/predjeskovic/neo4jrestapi/Neo4jRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.predjeskovic.neo4jrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neo4jRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/predjeskovic/neo4jrestapi/Neo4jRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.predjeskovic.neo4jrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neo4jRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
