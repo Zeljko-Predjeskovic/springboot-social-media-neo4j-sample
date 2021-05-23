e2bac296db708f949686763f98dac63f7a29af71
Zeljko Predjeskovic <zepr2022000@gmail.com>
2021-05-23 18:16:26 +0200

    updated README

--- src/main/java/com/predjeskovic/neo4jrestapi/persistence/PostRepository.java
+++ src/main/java/com/predjeskovic/neo4jrestapi/persistence/PostRepository.java
@@ -4,6 +4,9 @@ import com.predjeskovic.neo4jrestapi.domain.PostNode;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
+import java.nio.channels.Pipe;
+
 @Repository
 public interface PostRepository extends Neo4jRepository<PostNode,Long> {
+
 }
