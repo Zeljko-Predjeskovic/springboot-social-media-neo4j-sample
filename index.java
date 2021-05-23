c57a1c02dd1af0758300310202da2494564e1c7c
Zeljko Predjeskovic <zepr2022000@gmail.com>
2021-05-23 19:46:28 +0200

    new docs

---
+++ index.java
@@ -0,0 +1,27 @@
+/**
+ * # Social media api
+ * 
+ * by Zeljko Predjeskovic
+ * 
+ * May 23, 2021
+ *
+ * ### Introduction
+ *
+ * Hello, my name is Zeljko Predjeskovic and I am currently a student who
+ * studies computer science. I really love trying out new stuff especially when
+ * it comes to backend engineering. This project aims to build a web api that
+ * can be used for social media apps. Keep in mind that I am still a student and
+ * that this project will be far beyond from perfect. I will make sure to share
+ * my thinking so you can understand my processes.
+ *
+ * ### Overview
+ *
+ * Working my first time with Neo4j, a nosql graph database I am really excited.
+ * My plans and topics are:
+ *
+ * - A-Neo4j and its connection - B-Object Mapping - C-Repositories - D-Service
+ * - E-Presentation/Controller - F-Security - G-Web/Controller
+ *
+ * I will talk about each one of this section on how I write the code, What
+ * problems do I have and how am I solving them and of course a bit of theory.
+ */
