f02dd4e23583a8f053fac8e2dc0a86c9cf4189b6
Zeljko Predjeskovic <zepr2022000@gmail.com>
2021-05-28 00:37:13 +0200

    indexController DONE

--- src/main/java/com/predjeskovic/neo4jrestapi/webapp/IndexController.java
+++ src/main/java/com/predjeskovic/neo4jrestapi/webapp/IndexController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     @RequestMapping("/")
-    public String index(Model modes) {
+    public String index(Model model) {
         return "index.html";
     }
+
 }
--- src/main/java/com/predjeskovic/neo4jrestapi/webapp/PersonContoller.java
+++ src/main/java/com/predjeskovic/neo4jrestapi/webapp/PersonContoller.java
@@ -1,10 +1,16 @@
 package com.predjeskovic.neo4jrestapi.webapp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/people")
+@RequestMapping("/web")
 public class PersonContoller {
+
+    @RequestMapping("/people")
+    public String people(Model modes) {
+        return "person/people.html";
+    }
 
 }
